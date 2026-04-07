aafa58c81894df4594eca26eb01844de912471ec
alexnina01 <202112087@est.umss.edu>
2026-04-07 00:12:07 -0400

    agregar asteoroides

--- main.js
+++ main.js
@@ -14,8 +14,39 @@ let ship = {
 
 let bullets = [];
 let asteroids = [];
+function crearAsteroide() {
+    const radio = 20 + Math.random() * 30;
+    const lados = 5 + Math.floor(Math.random() * 4);
+    return {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radio: radio,
+        vx: (Math.random() - 0.5) * 2,
+        vy: (Math.random() - 0.5) * 2,
+        rotacion: 0,
+        velRotacion: (Math.random() - 0.5) * 0.03,
+        puntos: Array.from({ length: lados }, (_, i) => {
+            const angulo = (i / lados) * Math.PI * 2 + (Math.random() - 0.5) * 0.8;
+            const r = 0.4 + Math.random() * 0.6;
+            return { x: Math.cos(angulo) * r, y: Math.sin(angulo) * r };
+        })
+    };
+}
+
+asteroids = Array.from({ length: 8 }, crearAsteroide);
 
 function update() {
+    function update() {
+    asteroids.forEach(a => {
+        a.x += a.vx;
+        a.y += a.vy;
+        a.rotacion += a.velRotacion;
+        if (a.x < -a.radio)                 a.x = canvas.width  + a.radio;
+        if (a.x >  canvas.width  + a.radio) a.x = -a.radio;
+        if (a.y < -a.radio)                 a.y = canvas.height + a.radio;
+        if (a.y >  canvas.height + a.radio) a.y = -a.radio;
+    });
+}
 
 }
 
@@ -29,6 +60,23 @@ function draw() {
     ctx.lineTo(ship.x + 15, ship.y + 20);
     ctx.closePath();
     ctx.stroke();
+
+    asteroids.forEach(a => {
+        ctx.save();
+        ctx.translate(a.x, a.y);
+        ctx.rotate(a.rotacion);
+        ctx.strokeStyle = "#fff";
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        a.puntos.forEach((p, i) => {
+            i === 0
+                ? ctx.moveTo(p.x * a.radio, p.y * a.radio)
+                : ctx.lineTo(p.x * a.radio, p.y * a.radio);
+        });
+        ctx.closePath();
+        ctx.stroke();
+        ctx.restore();
+    });
 }
 
 function gameLoop() {
