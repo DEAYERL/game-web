a23cffc2bfb4e06385b72f732e53a2978fcd221c
JhonatanDavidQuispePerez <davidqp02@gmail.com>
2026-04-15 21:33:37 -0400

    style: corregir indentación del código

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ canvas.height = window.innerHeight
 
 let gameState = 'menu'
 let ship = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
-  angle: 0,
-  vx: 0,
-  vy: 0
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    angle: 0,
+    vx: 0,
+    vy: 0
 }
 let bullets = []
 let asteroids = []
@@ -18,28 +18,28 @@ let keys = {}
 let score = 0
 
 document.addEventListener('keydown', (e) => {
-  keys[e.code] = true
+    keys[e.code] = true
 
-  if (e.code === 'Enter' && gameState === 'menu') {
+    if (e.code === 'Enter' && gameState === 'menu') {
     gameState = 'playing'
-  }
+    }
 
-  if (e.code === 'Space' && gameState === 'playing') {
+    if (e.code === 'Space' && gameState === 'playing') {
     disparar()
-  }
-  if (e.code === 'Enter' && gameState === 'gameover') {
+    }
+    if (e.code === 'Enter' && gameState === 'gameover') {
     reiniciarJuego()
-  }
+    }
 })
 
 document.addEventListener('keyup', (e) => {
-  keys[e.code] = false
+    keys[e.code] = false
 })
 
 function crearAsteroide() {
   const radio = 20 + Math.random() * 30
   const lados = 5 + Math.floor(Math.random() * 4)
-  return {
+    return {
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
     radio: radio,
@@ -52,50 +52,50 @@ function crearAsteroide() {
       const r = 0.4 + Math.random() * 0.6
       return { x: Math.cos(angulo) * r, y: Math.sin(angulo) * r }
     })
-  }
+    }
 }
 
 asteroids = Array.from({ length: 8 }, crearAsteroide)
 
 function drawMenu() {
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = 'white'
-  ctx.font = 'bold 64px monospace'
-  ctx.textAlign = 'center'
-  ctx.fillText('ASTEROIDS', canvas.width / 2, canvas.height / 2 - 40)
-  ctx.font = '24px monospace'
-  ctx.fillStyle = '#aaa'
-  ctx.fillText('Presiona ENTER para jugar', canvas.width / 2, canvas.height / 2 + 30)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'white'
+    ctx.font = 'bold 64px monospace'
+    ctx.textAlign = 'center'
+    ctx.fillText('ASTEROIDS', canvas.width / 2, canvas.height / 2 - 40)
+    ctx.font = '24px monospace'
+    ctx.fillStyle = '#aaa'
+    ctx.fillText('Presiona ENTER para jugar', canvas.width / 2, canvas.height / 2 + 30)
 }
 
 function update() {
-  if (gameState !== 'playing') return
+    if (gameState !== 'playing') return
 
-  if (keys['ArrowLeft']) {
+    if (keys['ArrowLeft']) {
     ship.angle -= 0.05
-  }
-  if (keys['ArrowRight']) {
+    }
+    if (keys['ArrowRight']) {
     ship.angle += 0.05
-  }
+    }
 
-  if (keys['ArrowUp']) {
+    if (keys['ArrowUp']) {
     ship.vx += Math.cos(ship.angle - Math.PI / 2) * 0.1
     ship.vy += Math.sin(ship.angle - Math.PI / 2) * 0.1
-  }
+    }
 
-  ship.x += ship.vx
-  ship.y += ship.vy
+    ship.x += ship.vx
+    ship.y += ship.vy
 
   ship.vx *= 0.99
   ship.vy *= 0.99
 
-  if (ship.x < 0) ship.x = canvas.width
-  if (ship.x > canvas.width) ship.x = 0
-  if (ship.y < 0) ship.y = canvas.height
-  if (ship.y > canvas.height) ship.y = 0
+    if (ship.x < 0) ship.x = canvas.width
+    if (ship.x > canvas.width) ship.x = 0
+    if (ship.y < 0) ship.y = canvas.height
+    if (ship.y > canvas.height) ship.y = 0
 
-  asteroids.forEach((a) => {
+    asteroids.forEach((a) => {
     a.x += a.vx
     a.y += a.vy
     a.rotacion += a.velRotacion
@@ -104,40 +104,40 @@ function update() {
     if (a.x > canvas.width + a.radio) a.x = -a.radio
     if (a.y < -a.radio) a.y = canvas.height + a.radio
     if (a.y > canvas.height + a.radio) a.y = -a.radio
-  })
-  bullets.forEach((b, i) => {
+    })
+    bullets.forEach((b, i) => {
     b.x += Math.cos(b.angle - Math.PI / 2) * b.speed
     b.y += Math.sin(b.angle - Math.PI / 2) * b.speed
 
     if (b.x < 0 || b.x > canvas.width || b.y < 0 || b.y > canvas.height) {
-      bullets.splice(i, 1)
+        bullets.splice(i, 1)
     }
-  })
-  checkColisiones()
+    })
+    checkColisiones()
 }
 
 function draw() {
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = 'white'
-  ctx.font = '20px monospace'
-  ctx.textAlign = 'left'
-  ctx.fillText('Puntaje: ' + score, 20, 40)
-  ctx.save()
-  ctx.translate(ship.x, ship.y)
-  ctx.rotate(ship.angle)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'white'
+    ctx.font = '20px monospace'
+    ctx.textAlign = 'left'
+    ctx.fillText('Puntaje: ' + score, 20, 40)
+    ctx.save()
+    ctx.translate(ship.x, ship.y)
+    ctx.rotate(ship.angle)
 
-  ctx.strokeStyle = 'white'
-  ctx.beginPath()
-  ctx.moveTo(0, -20)
-  ctx.lineTo(-15, 20)
-  ctx.lineTo(15, 20)
-  ctx.closePath()
-  ctx.stroke()
+    ctx.strokeStyle = 'white'
+    ctx.beginPath()
+    ctx.moveTo(0, -20)
+    ctx.lineTo(-15, 20)
+    ctx.lineTo(15, 20)
+    ctx.closePath()
+    ctx.stroke()
 
-  ctx.restore()
+    ctx.restore()
 
-  asteroids.forEach((a) => {
+    asteroids.forEach((a) => {
     ctx.save()
     ctx.translate(a.x, a.y)
     ctx.rotate(a.rotacion)
@@ -150,99 +150,99 @@ function draw() {
     ctx.closePath()
     ctx.stroke()
     ctx.restore()
-  })
+    })
 
-  bullets.forEach((b) => {
+    bullets.forEach((b) => {
     ctx.fillStyle = 'white'
     ctx.beginPath()
     ctx.arc(b.x, b.y, 3, 0, Math.PI * 2)
     ctx.fill()
-  })
+    })
 }
 function checkColisiones() {
-  for (let i = bullets.length - 1; i >= 0; i--) {
+    for (let i = bullets.length - 1; i >= 0; i--) {
     for (let j = asteroids.length - 1; j >= 0; j--) {
-      const b = bullets[i]
-      const a = asteroids[j]
-      const dx = b.x - a.x
-      const dy = b.y - a.y
+        const b = bullets[i]
+        const a = asteroids[j]
+        const dx = b.x - a.x
+        const dy = b.y - a.y
       const distancia = Math.sqrt(dx * dx + dy * dy)
 
-      if (distancia < a.radio) {
+        if (distancia < a.radio) {
         bullets.splice(i, 1) // desaparece la bala
         asteroids.splice(j, 1) // desaparece el asteroide
         score += 10
         break
-      }
+        }
     }
-  }
-  // Nave vs Asteroide
-  for (let j = 0; j < asteroids.length; j++) {
+    }
+
+    for (let j = 0; j < asteroids.length; j++) {
     const a = asteroids[j]
     const dx = ship.x - a.x
     const dy = ship.y - a.y
     const distancia = Math.sqrt(dx * dx + dy * dy)
 
     if (distancia < a.radio + 15) {
-      gameState = 'gameover'
-      break
+        gameState = 'gameover'
+        break
     }
-  }
+    }
 }
 
 function disparar() {
-  bullets.push({
+    bullets.push({
     x: ship.x,
     y: ship.y,
     angle: ship.angle,
     speed: 5
-  })
+    })
 }
 
 document.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter' && gameState === 'menu') {
+    if (e.key === 'Enter' && gameState === 'menu') {
     gameState = 'playing'
-  }
+    }
 })
 
 function gameLoop() {
-  if (gameState === 'menu') {
+    if (gameState === 'menu') {
     drawMenu()
-  } else if (gameState === 'playing') {
+    } else if (gameState === 'playing') {
     update()
     draw()
-  } else if (gameState === 'gameover') {
+    } else if (gameState === 'gameover') {
     drawGameOver()
-  }
-  requestAnimationFrame(gameLoop)
+    }
+    requestAnimationFrame(gameLoop)
 }
 function drawGameOver() {
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-  ctx.fillStyle = 'red'
-  ctx.font = 'bold 72px monospace'
-  ctx.textAlign = 'center'
-  ctx.fillText('GAME OVER', canvas.width / 2, canvas.height / 2 - 60)
+    ctx.fillStyle = 'red'
+    ctx.font = 'bold 72px monospace'
+    ctx.textAlign = 'center'
+    ctx.fillText('GAME OVER', canvas.width / 2, canvas.height / 2 - 60)
 
-  ctx.fillStyle = 'white'
-  ctx.font = '32px monospace'
-  ctx.fillText('Puntaje: ' + score, canvas.width / 2, canvas.height / 2 + 10)
+    ctx.fillStyle = 'white'
+    ctx.font = '32px monospace'
+    ctx.fillText('Puntaje: ' + score, canvas.width / 2, canvas.height / 2 + 10)
 
-  ctx.fillStyle = '#aaa'
-  ctx.font = '22px monospace'
-  ctx.fillText('Presiona ENTER para reiniciar', canvas.width / 2, canvas.height / 2 + 60)
+    ctx.fillStyle = '#aaa'
+    ctx.font = '22px monospace'
+    ctx.fillText('Presiona ENTER para reiniciar', canvas.width / 2, canvas.height / 2 + 60)
 }
 
 function reiniciarJuego() {
-  ship.x = canvas.width / 2
-  ship.y = canvas.height / 2
-  ship.angle = 0
-  ship.vx = 0
-  ship.vy = 0
-  bullets = []
-  asteroids = Array.from({ length: 8 }, crearAsteroide)
-  score = 0
-  gameState = 'playing'
+    ship.x = canvas.width / 2
+    ship.y = canvas.height / 2
+    ship.angle = 0
+    ship.vx = 0
+    ship.vy = 0
+    bullets = []
+    asteroids = Array.from({ length: 8 }, crearAsteroide)
+    score = 0
+    gameState = 'playing'
 }
 gameLoop()
